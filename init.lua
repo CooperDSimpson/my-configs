6351696c4b83fe785b529bf35f94f119961d4605
CooperDSimpson <92061104+CooperDSimpson@users.noreply.github.com>
2025-09-13 13:12:55 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
 -- bootstrap lazy.nvim
-
-oufheofhauehof
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -296,4 +294,5 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     pcall(vim.treesitter.start, 0)
   end,
 })
+
 
